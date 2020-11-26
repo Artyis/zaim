9bca815c33d7c9fade4d8a396984ba81c6c83f38
Artyis <artygor@gmail.com>
2020-11-26 17:45:34 +0400

    Правки по css

--- block/footer.php
+++ block/footer.php
@@ -8,12 +8,14 @@
 </p>
 <p class="footer_text"> zaim-zaym.ru - Россия, 2020г.</p>
 </footer>
+<div class="ppop">
 <div id="popup" style="position:fixed;">
   <div class="for_popup container">
     <p class="popup_txt">Понравился наш сайт?! Добавьте его в закладки чтобы не потерять!;)</p>
     <a href="#" class="js-bookmark button" title="Добавить в закладки">Добавить в закладки</a>
     <a id="setCookie" class="close" href="#" title="Закрыть" onclick="document.getElementById('popup').style.display='none'; return false;"></a>
   </div>
+</div>
 </div>
 <script type="text/javascript">
 $(document).ready(function(){
@@ -26,7 +28,7 @@ if ( $.cookie("popup") == null )
 {
 setTimeout(function(){
 $("#popup").show();
-}, 4000)
+}, 8000)
 }
 else { $("#popup").hide();
 }
