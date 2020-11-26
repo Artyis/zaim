1c2d7d1a6bfce2ee19c73854d42b28035d8fd21d
Artyis <artygor@gmail.com>
2020-11-26 16:32:08 +0400

    seo аналитика

--- analitics.php
+++ analitics.php
@@ -5,3 +5,5 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NJFTH4T');</script>
 <!-- End Google Tag Manager -->
+<meta name="yandex-verification" content="0a43c2ed78f0404e" />
+<meta name="google-site-verification" content="HijYj4Tz5MCsM3omNuRPJbR0Ghm4jloscPHT34N_RDk" />
