2169a29f0934971e5498db5b40c9edf6d9bbf219
Artyis <artygor@gmail.com>
2020-12-03 19:03:27 +0400

    хлам

--- xlam.php
+++ xlam.php
@@ -1,13 +1,6 @@
 <!--liveint-->
 <div class="" style="display:none;">
-  <script>(function(d,s){d.getElementById("licnt08B2").src="//counter.yadro.ru/hit?t44.6;r"+escape(d.referrer)+((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+(s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+";h"+escape(d.title.substring(0,150))+";"+Math.random()})(document,screen)</script>
-  <!--hotlog-->
-  <script type="text/javascript"> var hot_s = document.createElement('script');
-  hot_s.type = 'text/javascript'; hot_s.async = true;
-  hot_s.src = 'http://js.hotlog.ru/dcounter/2592491.js';
-  hot_d = document.getElementById('hotlog_dyn');
-  hot_d.appendChild(hot_s);
-  </script>
+  
   <a href='http://modnayamoda.ru/modnye-zvezdy/' title='Модные звезды' rel="nofollow" >Модные звезды</a>
   <a rel="nofollow" href="https://webplus.info/index.php?page=47&add_top_url=191625">Каталог webplus.info</a>
 </div>
