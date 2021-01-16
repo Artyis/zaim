fac9044a86045e277eff17a3ae21a7980506f3ca
Artyis <artygor@gmail.com>
2021-01-16 12:40:44 +0400

    Сралинк

--- ajax/upd_loandb.php
+++ ajax/upd_loandb.php
@@ -1,30 +1,4 @@
 <?php
-function fn_print_r()
-{
-	static $count = 0;
-	$args = func_get_args();
-
-	if (!empty($args)) {
-		echo '<ol style="font-family: Courier; font-size: 12px; border: 1px solid #dedede; background-color: #efefef; float: left; padding-right: 20px;">';
-		foreach ($args as $k => $v) {
-			$v = htmlspecialchars(print_r($v, true));
-			if ($v == '') {
-				$v = '    ';
-		}
-
-			echo '<li><pre>' . $v . "\n" . '</pre></li>';
-		}
-		echo '</ol><div style="clear:left;"></div>';
-	}
-	$count++;
-}
-function fn_print_die()
-{
-	$args = func_get_args();
-	call_user_func_array('fn_print_r', $args);
-	die();
-}
-
 $id = $_POST['id'];
 $mfo = trim(filter_var($_POST['mfo']),FILTER_SANITIZE_STRING);
 $cpalink = trim(filter_var($_POST['cpalink']));
@@ -35,8 +9,6 @@ $sumto = filter_var(trim($_POST['sumto']),FILTER_SANITIZE_NUMBER_INT);
 $age = filter_var(trim($_POST['age']),FILTER_SANITIZE_NUMBER_INT);
 $chbx = filter_var($_POST['checkbox']);
 $txt = trim(filter_var($_POST['txt']),FILTER_SANITIZE_STRING);
-
-fn_print_die($cpalink);
 
 include 'connectdb.php';
 
